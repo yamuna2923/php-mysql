2fcf68ae76b17297d923ff8141ca984e03977fab
yamuna2923 <yamun6254167@gmail.com>
2020-10-30 03:06:20 +0530

    create connection

---
+++ insert1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <body>
+        <form method="post" action="process1.php">
+                regno:<br>
+                <input type="number" name="regno">
+                <br>
+                int301:<br>
+                <input type="number" name="int301">
+                <br>
+                cse515:<br>
+                <input type="number" name="cse515">
+                <br>
+                cse506:<br>
+                <input type="number" name="cse506">
+                <br><br>
+                cse501:<br>
+                <input type="number" name="cse501">
+                <br><br>
+                cse407:<br>
+                <input type="number" name="cse407">
+                <br><br>
+
+
+                <input type="submit" name="save" value="submit">
+        </form>
+  </body>
+</html>
+
+
+
+
